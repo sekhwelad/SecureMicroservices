3074d5124409531af0c2ca9f5e941046a2b3ec55
sekhwelad <25sekhwelad@gmail.com>
2023-04-02 00:30:18 +0200

    Secure Movie API with Identity Server

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -12,13 +12,22 @@ namespace IdentityServer
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
-
+                new Client
+                {
+                    ClientId="movieClient",
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes={ "movieAPI" }
+                }
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
            new ApiScope[]
            {
-
+               new ApiScope("movieAPI","Movies API")
            };
 
         public static IEnumerable<ApiResource> ApiResources =>
